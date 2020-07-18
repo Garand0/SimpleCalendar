2611e2732c86940274c2622334de84ce620fc7a5
Vakula Maksim <mvakyla@appkode.ru>
2020-07-18 10:56:49 +0300

    Initial Commit

---
+++ Calendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calendar
+//
+//  Created by Maksim Vakula on 7/18/20.
+//  Copyright © 2020 Maksim Vakula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
